6a7bb8ad0de4f724e019b7ede55f3d9836bbbb4b
Minato Sato <0zw3253t2c0390a@gmail.com>
2024-12-22 16:28:08 +0900

    fix build err

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,34 +4,15 @@ import Link from './Link'
 const Card = ({ title, description, imgSrc, href }) => (
   <div className="md max-w-[544px] p-4 md:w-1/2">
     <div
-      className={`${
-        imgSrc && 'h-full'
+      className={`{
+        'h-full' 
       }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
     >
-      {imgSrc &&
-        (href ? (
-          // <Link href={href} aria-label={`Link to ${title}`}>
-            <Image
-              alt={title}
-              src={imgSrc}
-              // className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
-            />
-          // </Link>
-        ) : (
-          <Image
-            alt={title}
-            src={imgSrc}
-            // className="object-cover object-center md:h-36 lg:h-48"
-            width={544}
-            height={306}
-          />
-        ))}
-        <div className="p-6">
-          <h2 className="mb-2 text-2xl font-bold">{title}</h2>
-          <p className="mb-4 text-gray-700">{description}</p>
-        </div>
+      <Image alt={title} src={imgSrc} width={544} height={306} />
+      <div className="p-6">
+        <h2 className="mb-2 text-2xl font-bold">{title}</h2>
+        <p className="mb-4 text-gray-700">{description}</p>
+      </div>
     </div>
   </div>
 )
